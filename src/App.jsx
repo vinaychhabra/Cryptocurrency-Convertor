7e2f8e74a55573c7ed092ca44fe42a9a75c8b1a6
vinaychhabra <vchhabra10feb@gmail.com>
2024-04-04 05:16:29 -0400

    logic added for coversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
-
+import { useState } from "react";
+import { InputBox } from "./components"
+import useCurrencyInfo from "./hooks/useCurrencyInfo"
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("usd");
+  const [toCurrency,setToCurrency] = useState("inr")
   const [count, setCount] = useState(0)
+  const [convertedAmount, setConvertedAmount] = useState(0);  
+  const currencyInfo = useCurrencyInfo(1);
 
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFromCurrency(toCurrency)
+    setToCurrency(fromCurrency)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+const convert = () => {
+  setConvertedAmount(amount * currencyInfo[to])
+}
+ 
   return (
     <>
+    
     </>
   )
 }
